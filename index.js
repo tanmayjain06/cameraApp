b80ba4eb6e9a0e3df4860bca165a5de72957a8c0
mejoiya <ravikantjoiya2005@gmail.com>
2024-12-29 14:04:16 +0530

    first commit

--- index.js
+++ index.js
@@ -2,8 +2,17 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import React from 'react';
+import { AppRegistry, SafeAreaView } from 'react-native';
+import CameraScreen from './src/screens/CameraScreen'; // Import CameraScreen component
+import { name as appName } from './app.json';
+
+const App = () => {
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <CameraScreen />
+    </SafeAreaView>
+  );
+};
 
 AppRegistry.registerComponent(appName, () => App);
